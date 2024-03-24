96186c346a642793f020dceec3260d4e1e5ca5ee
konika <konikajain1572001@gmail.com>
2024-03-25 00:35:23 +0530

    second commit

--- JAVASCRIPT/index.js
+++ JAVASCRIPT/index.js
@@ -1,1 +1,43 @@
-document.getElementById('1').style.color="red";
+let number = Math.floor(Math.random() * 20 +1);
+console.log(number);
+
+var count=0;
+var high_score=0;
+document.querySelector('.checkbtn').addEventListener('click',function(){
+    
+    const user_number = Number(document.querySelector('.guess').value);
+    
+    console.log(user_number);
+
+    if(user_number===number){
+        document.querySelector('.message').textContent="Correct Guess";
+        document.querySelector('.number').textContent=user_number;
+    }
+    else if(user_number==''){
+        document.querySelector('.message').textContent="Guess the number";
+    }
+    else if(user_number<number){
+        document.querySelector('.message').textContent="Guess a big number";
+        count++;
+    }
+    else if(user_number>number){
+        document.querySelector('.message').textContent="Guess a small number";
+        count++;
+    }
+
+    console.log("the count is" + count);
+
+    var score = document.querySelector('.score span').textContent=count;
+    // var score = document.querySelector('.score span').appendChild=count;
+    console.log("the score is" + score);
+    
+    if(score>high_score){
+        high_score=score;
+        document.querySelector('.high_score span').textContent=high_score;
+    }
+    else{
+        document.querySelector('.high_score span').textContent=high_score;
+    }
+
+    console.log("the high_score is" + high_score);
+})
